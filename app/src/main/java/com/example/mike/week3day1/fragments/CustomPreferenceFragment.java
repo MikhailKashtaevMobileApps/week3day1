054eb6cecc48a567e0825ab46ee0958c45756ab2
MikhailKashtaevMobileApps <mikhail.kashtaev@mobileappscompany.us>
2018-11-05 19:40:13 -0500

    Added dependency

--- app/src/main/java/com/example/mike/week3day1/fragments/CustomPreferenceFragment.java
+++ app/src/main/java/com/example/mike/week3day1/fragments/CustomPreferenceFragment.java
@@ -1,9 +1,14 @@
 package com.example.mike.week3day1.fragments;
 
+import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 public class CustomPreferenceFragment extends PreferenceFragmentCompat {
 
 
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
 
+    }
 }
--- app/src/main/java/com/example/mike/week3day1/fragments/PersonFragment.java
+++ app/src/main/java/com/example/mike/week3day1/fragments/PersonFragment.java
@@ -19,17 +19,10 @@ import java.util.List;
 /**
  * A fragment representing a list of Items.
  * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
  * interface.
  */
 
 public class PersonFragment extends Fragment {
-
-    // TODO: Customize parameter argument names
-
-    private List<Person> people;
-
-    // TODO: Customize parameters
     private int mColumnCount = 1;
 
     /**
@@ -47,11 +40,6 @@ public class PersonFragment extends Fragment {
         args.putSerializable( "people", (Serializable) people);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -75,18 +63,4 @@ public class PersonFragment extends Fragment {
         return view;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(Person item);
-    }
 }
